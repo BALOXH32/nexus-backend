d7d0a12e5098d11f4ddf66753ad6b162d33fca58
BALOXH32 <alimuradbaloxh@gmail.com>
2026-03-23 10:37:25 +0500

    Update progressRoutes.js

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const progressController = require('../controllers/progressController');
 
+// Get progress
+router.get('/students/:studentId/courses/:courseId', progressController.getCourseProgress);
+
+// Save progress
+router.post('/save', progressController.saveProgress);
+
+// Mark complete
+router.post('/complete', progressController.markComplete);
+
+module.exports = router;
