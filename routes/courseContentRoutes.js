6d1f420082d6ce1112c111afe3d5d92b11f01b4d
BALOXH32 <alimuradbaloxh@gmail.com>
2026-03-23 10:36:33 +0500

    Update courseContentRoutes.js

--- routes/courseContentRoutes.js
+++ routes/courseContentRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const courseContentController = require('../controllers/courseContentController');
 
+// Get course curriculum
+router.get('/:courseId/curriculum', courseContentController.getCurriculum);
+
+// Module management (admin only)
+router.post('/:courseId/modules', courseContentController.addModule);
+router.put('/modules/:moduleId', courseContentController.updateModule);
+router.delete('/modules/:moduleId', courseContentController.deleteModule);
+
+module.exports = router;
