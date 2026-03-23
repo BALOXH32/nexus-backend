59edc8c25b23373bf286f7d677b75c56cb04dfe6
BALOXH32 <alimuradbaloxh@gmail.com>
2026-03-23 10:37:09 +0500

    Update accessRoutes.js

--- routes/accessRoutes.js
+++ routes/accessRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const accessController = require('../controllers/accessController');
 
+// Check access
+router.get('/students/:studentId/courses/:courseId', accessController.checkAccess);
+
+// Grant access (admin only)
+router.post('/grant', accessController.grantAccess);
+
+module.exports = router;
